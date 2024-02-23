b87b1c90cd3056234c7569f68750468f9b14174b
Maria Campo Martins <mariacampomartins@gmail.com>
2024-02-23 11:06:12 -0800

    Added posting feature using Firestore

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../firebase"; 
 import Navbar from "../Navbar";
+import Button from "../Button";
 import "./HomePage.css"
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    };
+    getPosts(); 
+  });
   return (
     <div>
       <Navbar />
-      <div>HomePage</div>
+      <Button onClick={() => navigate("/createpost")}>Create a Post</Button>
+      <div className="homepage"> {postList.map((post) => {
+        return (
+        <div className="Post"> 
+          <div className="postHeader"> 
+            <h3>{post.title}</h3> 
+          </div>
+          <div className="postText"> 
+            {post.postText}
+          </div>
+        </div>
+        );
+      })} 
+      </div>
     </div>
   )
-}
+};
