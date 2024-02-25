df734fd7ca930063ea62e29039f21b490bb37e71
AyahKash <ayahkashmoney@gmail.com>
2024-02-24 22:16:41 -0800

    updated CreatePost.js to include form, tested while firebase down, updated style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import SignIn from "./components/auth/SignIn"
 import SignUp from "./components/auth/SignUp"
@@ -9,17 +9,38 @@ import { Settings } from "./components/pages/Settings";
 import { Profile } from "./components/pages/Profile";
 import { CreatePost } from "./components/pages/CreatePost"
 
+  //   {
+  //     title: "Post One",
+  //     postText:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer diam dui, pellentesque a pretium nec, consequat et risus. Aliquam posuere mollis mauris. Sed eget sapien ut quam condimentum luctus...",
+  //     id: "1",
+  //   }
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  // setPosts([
+  //  {
+  //    title: "Post One",
+  //   postText:
+  //    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer diam dui, pellentesque a pretium nec, consequat et risus. Aliquam posuere mollis mauris. Sed eget sapien ut quam condimentum luctus...",
+  //    id: "1",
+  //     }
+  //    ]);
+  //<Route path="/homepage" element={<React.Fragment><HomePage/><AuthDetails /></React.Fragment>} />
+//<Route path="/createpost" element={<CreatePost/>}/>
+
+  //        <Route path="/createpost" element={<CreatePost updatePosts={setPosts}/>}/>
+  //        <Route path="/homepage" element={<React.Fragment><HomePage postsList={posts}/><AuthDetails /></React.Fragment>} />
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/homepage" element={<React.Fragment><HomePage /><AuthDetails /></React.Fragment>} />
+        <Route path="/homepage" element={<React.Fragment><HomePage postsList={posts}/><AuthDetails /></React.Fragment>} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/createpost" element={<CreatePost/>}/>
-      </Routes>
+        <Route path="/createpost" element={<CreatePost updatePosts={setPosts}/>}/>      </Routes>
     </div>
   );
 }
