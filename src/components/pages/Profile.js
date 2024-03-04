ec610f4a99356501369528604dc51ab658caf1ed
masonamp <155843839+masonamp@users.noreply.github.com>
2024-03-03 16:19:26 -0800

    adds friendsPage
    Adds friendsPage component that includes a list of friends, a list of friend requests, and a form to send a new friend request given a user's email. Includes the logic for sending friend requests, accepting/rejecting, and removing a friend.

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../Navbar";
+import Button from "../Button";
+import { useNavigate } from "react-router-dom";
 import "./Profile.css";
 
 export const Profile = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Navbar />
       <div>Profile Page</div>
+      <Button onClick={() => navigate("/profile/friends")}> Friends </Button>
     </div>
   );
 };
