580daafe8e1eecd4aefcb05d90bbff1f0ab262e6
Maria Campo Martins <mariacampomartins@gmail.com>
2024-03-08 10:25:41 -0800

    Updating firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -18,33 +18,4 @@ import {getStorage} from "firebase/storage"
     const app = initializeApp(firebaseConfig);
     export const auth = getAuth(app);
     export const storage = getStorage();
-    export const db = getFirestore();  //get database
-    
-    const firebaseConfig = {
-    apiKey: "AlzaSyDG3PBDHtliQfb7mNqAoD6rx1EMN7C8IOI",
-    authDomain: "bytebod-9d1cf.firebaseapp.com",
-    projectId: "bytebod-9d1cf",
-    storageBucket: "bytebod-9d1cf.appspot.com",
-    messagingSenderId: "545880527544",
-    appId: "1:545880527544:web:9d4e618b4e5f017da09a4d"
-    };
-    
-    const app = initializeApp(firebaseConfig);
-    export const auth = getAuth(app);
-    export const storage = getStorage();
-    export const db = getFirestore();  //get database
-    
-    const firebaseConfig = {
-    apiKey: "AlzaSyDG3PBDHtliQfb7mNqAoD6rx1EMN7C8IOI",
-    authDomain: "bytebod-9d1cf.firebaseapp.com",
-    projectId: "bytebod-9d1cf",
-    storageBucket: "bytebod-9d1cf.appspot.com",
-    messagingSenderId: "545880527544",
-    appId: "1:545880527544:web:9d4e618b4e5f017da09a4d"
-    };
-    
-    const app = initializeApp(firebaseConfig);
-    export const auth = getAuth(app);
-    export const storage = getStorage();
-    export const db = getFirestore();  //get database
-    
+    export const db = getFirestore();  //get database  
