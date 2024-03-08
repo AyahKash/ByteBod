483c35b9d9826b11e1d7bf91c6ae3183b526de86
anushkanayak15 <157661021+anushkanayak15@users.noreply.github.com>
2024-03-07 19:33:48 -0800

    minor commit to my local branch

--- src/firebase.js
+++ src/firebase.js
@@ -34,3 +34,17 @@ import {getStorage} from "firebase/storage"
     export const storage = getStorage();
     export const db = getFirestore();  //get database
     
+    const firebaseConfig = {
+    apiKey: "AlzaSyDG3PBDHtliQfb7mNqAoD6rx1EMN7C8IOI",
+    authDomain: "bytebod-9d1cf.firebaseapp.com",
+    projectId: "bytebod-9d1cf",
+    storageBucket: "bytebod-9d1cf.appspot.com",
+    messagingSenderId: "545880527544",
+    appId: "1:545880527544:web:9d4e618b4e5f017da09a4d"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+    export const auth = getAuth(app);
+    export const storage = getStorage();
+    export const db = getFirestore();  //get database
+    
