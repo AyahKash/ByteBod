a370b244cacdf48ca51fda3fed6fcf9a7a992050
shilpa-bo <131549352+shilpa-bo@users.noreply.github.com>
2024-03-15 16:29:44 -0700

    Adding comments

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,19 +10,39 @@ import {
 import { db } from "../firebase";
 import React, {useState } from "react";
 
+/**
+ * The SearchBar component provides a user interface for searching posts by workout type.
+ * It leverages React Bootstrap for styling and layout. The component interacts with Firebase Firestore
+ * to query posts based on the user's search input. On submission, it navigates to a search results page
+ * with the matching posts. The search is case-insensitive and whitespace-agnostic.
+ * 
+ * @returns {JSX.Element} A styled search bar that allows users to search for posts by workout type.
+ */
 
-/*
-Implements search bar using bootstrap, and allows a user to search through posts based off of workout
-type using query. searches through the post database on firebase
-*/
 export default function SearchBar() {
   const[postList, setPostList] = useState([]);
   const[workout, setWorkout] = useState("");
-  
+
+  /**
+   * Formats the given string by removing spaces and converting it to lowercase.
+   * This standardization helps in matching the search input with the database entries.
+   * 
+   * @param {string} string The input string to format.
+   * @returns {string} The formatted string.
+   */
 const formatString = (string) => {
   return string.replace(/\s/g, "").toLowerCase();
 };
-  
+
+
+  /**
+   * Handles the search operation when the search button is clicked. It prevents the default form submission behavior,
+   * formats the workout type input, constructs a query for the Firestore database, and fetches the matching documents.
+   * If no documents are found, it alerts the user. Otherwise, it updates the state with the found posts and navigates
+   * to the search results page.
+   * 
+   * @param {Event} event The event object representing the form submission.
+   */
 const getData = async (event) => {
   event.preventDefault();
   const formattedString = formatString(workout);
